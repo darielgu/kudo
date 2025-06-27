aae3e9e12bb91abd1f347d2fa3a28fa20b257c0c
Oliver Majano <olivermajano01@gmail.com>
2025-06-27 15:21:54 -0700

    crud opperations for board

--- kudo-backend/src/controller/board.js
+++ kudo-backend/src/controller/board.js
@@ -14,3 +14,35 @@ export const createBoard = async (req, res) => {
     }
 }
 
+export const getBoards = async (req, res) => {
+    try {
+        const boards = await prisma.board.findMany();
+        res.status(200).json(boards);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const getBoardById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const board = await prisma.board.findUnique({
+            where: { id: parseInt(id) }
+        });
+        res.status(200).json(board);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const deleteBoard = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const board = await prisma.board.delete({
+            where: { id: parseInt(id) }
+        });
+        res.status(200).json(board);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- kudo-backend/src/routes/boardRoutes.js
+++ kudo-backend/src/routes/boardRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { createBoard } from "../controller/board.js";
+import { createBoard, getBoards, getBoardById, deleteBoard } from "../controller/board.js";
 
 router.post("/", createBoard);
+router.get("/", getBoards);
+router.get("/:id", getBoardById);
+router.delete("/:id", deleteBoard);
 
 export default router;
