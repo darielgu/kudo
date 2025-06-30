1b86de17820dd7f1d424bed14323c4230c5e2608
darielgu <darielgutierrez332@gmail.com>
2025-06-29 21:55:52 -0700

    goated insight

--- kudo-frontend/src/components/SearchBar.jsx
+++ kudo-frontend/src/components/SearchBar.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 import { Typography, Button, TextField, Box } from "@mui/material";
 
 const SearchBar = () => {
-
+  // need to pass the value in here to HomePage.jsx and sort | also don't think we need this component
   return (
-    <Box mt={2} sx = {{display: "flex", justifyContent: "center"}}>
-        <TextField
+    <Box mt={2} sx={{ display: "flex", justifyContent: "center" }}>
+      <TextField
         label="Search boards..."
         variant="outlined"
-        sx = {{
-            width: "70%"
+        sx={{
+          width: "70%",
         }}
-        />
+      />
     </Box>
   );
 };
