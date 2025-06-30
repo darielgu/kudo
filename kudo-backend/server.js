abe22c5e102919932a2333047a4e4c2f36d263e6
darielgu <darielgutierrez332@gmail.com>
2025-06-29 21:23:20 -0700

    allowed for cors : add frontend port in your .env

--- kudo-backend/server.js
+++ kudo-backend/server.js
@@ -1,9 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
+import express from "express";
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND = process.env.FRONTEND;
 import morgan from "morgan";
+import cors from "cors"; //need cors for FrontEnd connection
+const corsOptions = {
+  origin: `http://localhost:${FRONTEND}`, // Allow requests from FrontEnd port
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 import boardRoutes from "./src/routes/boardRoutes.js";
@@ -12,7 +18,6 @@ import cardRoutes from "./src/routes/cardRoutes.js";
 app.use("/board", boardRoutes);
 app.use("/card", cardRoutes);
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
